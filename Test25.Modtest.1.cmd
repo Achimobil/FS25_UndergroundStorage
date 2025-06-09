ccb6d5f036011919729f97eacd48ee6f7ae55ee5
Achim Kuntz <achimobil@hotmail.com>
2025-06-09 12:13:54 +0200

    übernahme was geht als object storage

--- Test25.Modtest.1.cmd
+++ Test25.Modtest.1.cmd
@@ -1,2 +1,2 @@
 "C:\Program Files\7-Zip\7z.exe" u -tzip "E:\LS25Profile\LS25ModTest\mods\FS25_UndergroundStorage.zip" -x@FileDefinition.txt
-start "" "C:\Program Files (x86)\Farming Simulator 2025\x64\FarmingSimulator2025Game.exe" -autoStartSavegameId 1 -cheats -profile E:\LS25Profile\LS25ModTest
+start "" "C:\Program Files (x86)\Farming Simulator 2025\x64\FarmingSimulator2025Game.exe" -autoStartSavegameId 1 -cheats -profile E:\LS25Profile\LS25ModTest -skipModUpdateDialog
