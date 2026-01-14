d98b7d907923f7852ba3459d04e5f99eb530e321
Achimobil <achimobil@hotmail.com>
2026-01-14 08:10:48 +0100

    Objekttrigger korrigiert #3

---
+++ Test25.NFMarsch.3.cmd
@@ -0,0 +1,2 @@
+"C:\Program Files\7-Zip\7z.exe" u -tzip "E:\LS25Profile\NFMarsch\mods\FS25_UndergroundStorage.zip" -x@FileDefinition.txt
+start "" "C:\Program Files (x86)\Farming Simulator 2025\x64\FarmingSimulator2025Game.exe" -autoStartSavegameId 3 -cheats -profile E:\LS25Profile\NFMarsch -skipModUpdateDialog
